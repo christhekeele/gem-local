4c14ea31164ef648f6ac6971e019f6192d7f2c72
Chris Keele <dev@chriskeele.com>
2016-05-03 02:43:24 -0500

    Add rebuild utility in case things get out of sync.

--- lib/rubygems/commands/local_command.rb
+++ lib/rubygems/commands/local_command.rb
@@ -26,7 +26,8 @@ show [gem]       | Displays all or a particular local gem configuration.
 remove <gem>     | Removes a local gem from `gem local` configuration.
 on [gem, ...]    | Turns local gem(s) on.
 off [gem, ...]   | Turns local gem(s) off.
-ignore           | Adds `.gemlocal` artifact to project .gitignore.
+rebuild          | Regenerates your `.gemlocal` from bundle config state.
+install          | Adds `.gemlocal` artifact to project `.gitignore`.
 help [cmd]       | Displays help information.
     ARGS
   end
@@ -53,7 +54,7 @@ help [cmd]       | Displays help information.
     if name and location and args.empty?
       setting = Setting.new(location)
       if bundler_add name, setting
-        update_config(name, :status, "on")
+        update_config(name, :status, setting.status)
       end
     else
       arity_error __method__
@@ -80,9 +81,8 @@ help [cmd]       | Displays help information.
 
   def remove(name = nil, *args)
     if name and args.empty?
-      new_config = config
-      new_config.delete(name)
-      write_config(new_config)
+      config.delete(name)
+      write_config(config)
     else
       arity_error __method__
     end
@@ -90,27 +90,18 @@ help [cmd]       | Displays help information.
   alias_method :delete, :remove
   
   def on(*names)
-    if names.empty?
-      config.each do |name, setting|
+    names = config.values if names.empty?
+    names.each do |name|
+      if setting = config[name]
         if bundler_add name, setting
           update_config(name, :status, "on")
-        end
-      end
-    else
-      names.each do |name|
-        if setting = config[name]
-          if bundler_add name, setting
-            update_config(name, :status, "on")
-          else
-            raise "Could not activate gem, make sure `bundle config local.#{name}` #{setting.location} succeeds"
-          end
         else
-          raise "`gem local on` could not find `#{name}` in:\n#{find_config}"
+          raise "Could not activate gem, make sure `bundle config local.#{name}` #{setting.location} succeeds"
         end
+      else
+        raise "`gem local on` could not find `#{name}` in:\n#{find_config}"
       end
-    # else
-    #   arity_error __method__
-    end    
+    end
   end
   alias_method :use, :on
   alias_method :activate, :on
@@ -119,31 +110,39 @@ help [cmd]       | Displays help information.
   alias_method :reactivate, :on
   
   def off(*names)
-    if names.empty?
-      config.each do |name, setting|
+    names = config.values if names.empty?
+    names.each do |name|
+      if setting = config[name]
         if bundler_remove name, setting
           update_config(name, :status, "off")
-        end
-      end
-    else
-      names.each do |name|
-        if setting = config[name]
-          if bundler_add name, setting
-            update_config(name, :status, "off")
-          else
-            raise "Could not deactivate gem, make sure `bundle config --delete local.#{name}` succeeds"
-          end
         else
-          raise "`gem local off` could not find `#{name}` in:\n#{find_config}"
+          raise "Could not deactivate gem, make sure `bundle config --delete local.#{name}` succeeds"
         end
+      else
+        raise "`gem local off` could not find `#{name}` in:\n#{find_config}"
       end
-    # else
-    #   arity_error __method__
-    end    
+    end
   end
   alias_method :ignore, :off
   alias_method :deactivate, :off
   alias_method :disable, :off
+  
+  def rebuild(*args)
+    if args.empty?
+      config = read_config.dup
+      clear_config_cache
+      File.open(find_config, "w") do |file|
+        config.each do |name, old_setting|
+          if setting = bundler_value(name, old_setting)
+            file.puts config_for(name, setting.location, setting.status)
+          end
+        end
+      end
+      show
+    else
+      arity_error __method__
+    end    
+  end
   
   def install(*args)
     if args.empty?
@@ -206,6 +205,12 @@ private
         arguments: "takes any number of arguments",
         aliases: %w[ignore deactivate disable],
       },
+      "rebuild" => {
+        description: "Regenerates your local `.gemlocal` file from the bundle config if they get out of sync.",
+        usage: "rebuild",
+        arguments: "takes zero arguments",
+        aliases: %w[],
+      },
       "install" => {
         description: "Adds `.gemlocal` and `.bundle` artifacts to project `.gitignore`",
         usage: "install",
@@ -216,6 +221,7 @@ private
         description: "Displays help information, either about `gem local` or a `gem local` subcommand.",
         usage: "help [cmd]",
         arguments: "takes zero or one arguments",
+        aliases: %w[],
       },
     }
   end
@@ -249,6 +255,10 @@ private
     info[:description]
   end
   
+  def config_for(name, location, status)
+    "%-3.3s #{name} #{location}" % status
+  end
+  
   def arity_error(cmd)
     info = cmds[cmd.to_s]
     raise "`gem local #{cmd}` #{info[:arguments]}" + "\n" + info_for(cmd)
@@ -259,19 +269,22 @@ private
   def config
     @config ||= read_config
   end
+  
+  def clear_config_cache
+    @config = nil
+  end
 
   def read_config
     File.open(find_config) do |file|
-      lines = file.readlines
       config = Hash[
-        lines.reject do |line|
+        file.readlines.reject do |line|
           line.start_with? "#" or line.strip.empty?
         end.map do |line|
           status, name, location, *args = line.strip.split
           if status and name and location and args.empty?
             [name, Setting.new(location, status)]
           else
-            raise "`gem local` config in `#{path}` is corrupt, each non-empty non-commented line must contain a status, a gem name, and the local path to that gem, separated by spaces\nerror at:\n#{line}"
+            raise "`gem local` config in `#{find_config}` is corrupt, each non-empty non-commented line must contain a status, a gem name, and the local path to that gem, separated by spaces\nerror at:\n#{line}"
           end
         end
       ]
@@ -299,17 +312,16 @@ private
   def write_config(config)
     File.open(find_config, "w") do |file|
       config.each do |name, setting|
-        file.puts "%-3.3s #{name} #{setting.location}" % setting.status
+        file.puts config_for(name, setting.location, setting.status)
       end
     end
   end
   
   def update_config(name, field, value)
-    new_config = config
-    new_setting = new_config[name]
+    new_setting = config[name]
     new_setting.send(:"#{field}=", value)
-    new_config[name] = new_setting
-    write_config new_config
+    config[name] = new_setting
+    write_config config
     puts show_setting_for(name, new_setting)
   end
   
@@ -321,6 +333,27 @@ private
   def bundler_remove(name, setting)
     Bundler.settings["local.#{name}"] = nil
     not Bundler.settings["local.#{name}"]
+  end
+  
+  def bundler_value(name, setting)
+    if new_setting = bundler_values[name]
+      new_setting
+    else
+      setting.status = "off"
+      setting
+    end
+  end
+  
+  def bundler_values
+    @bundler_values ||= Hash[
+      Bundler.settings.send(:load_config, Bundler.settings.send(:local_config_file)).select do |setting, value|
+        setting =~ /^BUNDLE_LOCAL__/
+      end.map do |setting, value|
+        [ setting[/^BUNDLE_LOCAL__(?<name>.*?)$/, :name].downcase, value ]
+      end.map do |name, location|
+        [ name, Setting.new(location, "on")]
+      end
+    ]
   end
   
 end
